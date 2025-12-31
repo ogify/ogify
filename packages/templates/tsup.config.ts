969f5d0a1895dd3ad39f381cc562883d812399b9
Hung Pham <phamdohung161@gmail.com>
2025-12-31 10:37:46 +0700

    feat(templates): implement basic template

--- packages/templates/tsup.config.ts
+++ packages/templates/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: {
     'minimalism/index': 'src/minimalism/index.ts',
+    'basic/index': 'src/basic/index.ts',
   },
   format: ['cjs', 'esm'],
   dts: true,
