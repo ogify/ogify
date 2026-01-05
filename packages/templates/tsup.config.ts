d7396d586c541e0e9b954eec544f280d8988327e
Hung Pham <phamdohung161@gmail.com>
2026-01-05 16:50:01 +0700

    refactor(templates): remove minimalism template and update basic as default

--- packages/templates/tsup.config.ts
+++ packages/templates/tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: {
-    'minimalism/index': 'src/minimalism/index.ts',
     'basic/index': 'src/basic/index.ts',
   },
   format: ['cjs', 'esm'],
